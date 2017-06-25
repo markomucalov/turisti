6c028f839d476748ef789397b8e91473159f4084
USER <USER@AsusK550L>
2017-06-25 18:23:29 +0200

    puko sam nacisto

---
+++ src/Turista.java
@@ -0,0 +1,11 @@
+
+public class Turista extends Korisnik {
+	
+	public Turista(){
+		
+	}
+	public Turista(String korisnicko_ime, String lozinka){
+		super(korisnicko_ime,lozinka);
+	}
+	
+}
---
+++ src/main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;;
+
+
+
+public class main{
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args){
+		
+		
+	}
+	
+}
+
+
