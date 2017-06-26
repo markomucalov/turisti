9612b3bae145784d7b26d00d5f62f142d2acfdc9
IgorMaj <majic753@gmail.com>
2017-06-26 15:55:27 +0200

    Dodat prozor za prikaz rezultata pretrage

--- src/view/GlavniProzor.java
+++ src/view/GlavniProzor.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
@@ -33,8 +35,18 @@ public class GlavniProzor extends JFrame {
 		meni();
 		panelPretrage();
 		pozadinaProzora();
+		omoguciIzlaz();
 
 	}
+	
+	//samo za test, uklanjamo ga posle
+		private void omoguciIzlaz(){
+			this.addWindowListener(new WindowAdapter() {
+		         public void windowClosing(WindowEvent windowEvent){
+		            System.exit(0);
+		         }        
+		      });
+		}
 
 	public void meni() {
 
---
+++ src/view/MiniTuraIzgled.java
@@ -0,0 +1,80 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import model.Korisnik;
+import model.Tura;
+
+public class MiniTuraIzgled extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	Tura tura;
+	int brojZnakova;
+	JButton prikaziTuruDugme;
+	Korisnik kor;
+	
+	public MiniTuraIzgled(Tura tura,int brojZnakovaOpisaZaPrikazati,Korisnik kor){
+		super();
+		this.tura = tura;
+		this.brojZnakova = brojZnakovaOpisaZaPrikazati;
+		this.kor = kor;
+		podesiIzgled();
+		podesiDugme();
+		
+	}
+
+	private void podesiDugme() {
+		this.prikaziTuruDugme.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				TuraProzor tp = new TuraProzor(tura,kor);
+				tp.setVisible(true);
+				
+			}});
+		
+	}
+
+	private void podesiIzgled() {
+		BorderLayout layout = new BorderLayout();
+		this.setLayout(layout);
+		JLabel naslov = new JLabel(this.tura.naslovTure);
+		naslov.setFont(new Font("Serif", Font.BOLD, 18));
+		JLabel kratakOpis  = new JLabel();
+		if(this.tura.opisTure.length() <=this.brojZnakova){
+			kratakOpis.setText(this.tura.opisTure);
+		}
+		else{
+			String text = tura.opisTure.substring(0, brojZnakova)+"...";
+			kratakOpis.setText(text);
+		}
+		
+		prikaziTuruDugme = new JButton("Prikazi turu");
+		GridLayout pomocniLayout = new GridLayout(1,2);
+		JPanel pomocniPanel = new JPanel();
+		pomocniPanel.setLayout(pomocniLayout);
+		pomocniPanel.add(naslov);
+		pomocniPanel.add(prikaziTuruDugme);
+		this.add(pomocniPanel,BorderLayout.PAGE_START);
+		this.add(kratakOpis, BorderLayout.CENTER);
+		this.add(new JLabel("	"), BorderLayout.PAGE_END);
+		
+	}
+	
+	
+	
+	
+	
+}
